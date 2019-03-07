8c10e809407eba1db9598b670fcacaa5c5a26785
Daniel Pope <mauve@mauveweb.co.uk>
2019-03-07 21:06:01 +0000

    More cars over time

--- trial.py
+++ trial.py
@@ -22,6 +22,7 @@ class MyApp(ShowBase):
         self.zoom = 1.2
 
         self.taskMgr.add(self.moveCameraTask, "moveCamera")
+        self.taskMgr.doMethodLater(2, self.next_car, "add car task")
 
         self.add_car()
 
@@ -29,6 +30,7 @@ class MyApp(ShowBase):
         car = self.loader.loadModel("truck.egg")
         car.reparentTo(self.render)
         car.setHpr(180, 0, 0)
+        car.setPos(0, self.car_y - (10 * len(self.cars)), 0)
         self.cars.append(car)
 
     def run(self):
@@ -36,11 +38,17 @@ class MyApp(ShowBase):
 
     def moveCameraTask(self, task):
         self.car_y += 0.01
-        self.cars[0].setPos(0, self.car_y, 0)
-        self.camera.setPos(10 * self.zoom, (self.car_y - 10) * self.zoom, 15 * self.zoom)
+        for i, car in enumerate(self.cars):
+            car.setPos(0, self.car_y - (10 * i), 0)
+        self.camera.setPos(10 * self.zoom, self.car_y - 10 * self.zoom, 15 * self.zoom)
         self.camera.setHpr(45, -45, 0)
         return Task.cont
 
+    def next_car(self, task):
+        self.add_car()
+        self.zoom += 1.2
+        task.delayTime = 2
+        return task.again
 
 if __name__ == '__main__':
     app = MyApp()
